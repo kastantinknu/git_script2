1efafdf6e112517af696b40bf420f700d9c393f8
Kostyantyn Zabolotnyy <kastantin.knu@gmail.com>
2022-09-13 21:06:44 +0200

    add fifth slide parts

--- slide05.sh
+++ slide05.sh
@@ -1,3 +1,15 @@
 echo "task9. other git commands"
-echo "command1 help"
+
+#get help
+echo "command1 help>"
 git help help #<command>
+git rm --help
+
+#show status and log:
+echo "command2 show status>"
+git status #show the working tree status
+echo "command3 show commit log>"
+git log #show commit logs
+echo "show files in the index"
+git ls-files -show
+
