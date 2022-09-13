f17d04f12e30082314b0af8854ed183d1a9787c0
Kostyantyn Zabolotnyy <kastantin.knu@gmail.com>
2022-09-13 10:09:34 +0200

    ##first slide

--- script.sh
+++ script.sh
@@ -1,5 +1,20 @@
-##
+##echo "user name:"
+#git config user.name
 ##git config --global user.name "Kostyantyn Zabolotnyy"
 ##git config --global user.email kastantin.knu@gmail.com
 ##git config --global core.editor emacs
+#echo "change dir to c:/user/kasta/source/repos/newrepo2"
+#cd c:/users/kasta/source/repos/newrepo2
+
+##first slide
+echo "show user name:"
+git config --global user.name
+echo
+echo "show user email:"
+git config --global user.email
+echo
+echo "config editor into emacs"
+git config --global core.editor emacs
+echo
+echo "show config list:"
 git config --list
