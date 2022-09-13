458fb989e04c088833bd9eb3fb1ab3722b8b8d2c
Kostyantyn Zabolotnyy <kastantin.knu@gmail.com>
2022-09-13 18:55:33 +0200

    add empty lines

--- slide04.sh
+++ slide04.sh
@@ -7,9 +7,14 @@ echo "show the working dir>"
 cmd //c cd
 echo "add all files ( "_.") to staging (index)"
 git add .
+
+echo
 echo "create a snapshot and save to a local repo"
 git commit
+#press i
 # ...some message
 #press esc then shift+: then wq then enter
+
+echo
 echo "send to remove repo"
 git push
